9557281aa24914d81d4a5a89b475c4f985c4479e
sidoriak <sidoryak@gmail.com>
2024-02-03 14:25:25 +0700

    task 0203

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
@@ -9,7 +9,8 @@ public class Solution {
     public static void main(String[] args) {
         int number = 546;
 
-        int lastDigit; //напишите тут ваш код
+        int lastDigit = number % 10;; //напишите тут ваш код
+
 
         System.out.println(lastDigit);
     }
