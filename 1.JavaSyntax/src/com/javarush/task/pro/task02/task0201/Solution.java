3814bd6c70828d6e7a4af14185c139e2822c4304
sidoriak <sidoryak@gmail.com>
2024-02-03 14:25:09 +0700

    task 0201

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -8,6 +8,10 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        int intValue = 1;
+        int numericValue = 2;
+        int size = 3;
+        int number = 4;
 
     }
 }
