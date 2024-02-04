de0a5594efd2746318e5ec4485c06d08c6fc88d9
sidoriak <sidoryak@gmail.com>
2024-02-04 14:06:11 +0700

    task 0206

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -4,6 +4,11 @@ public class Solution {
 
     public static void main(String[] args) {
         //напишите тут ваш код
+        String word = "word";
+        String phrase = "phrase";
+        String line = "line";
+        String text = "text";
+
 
     }
 }
