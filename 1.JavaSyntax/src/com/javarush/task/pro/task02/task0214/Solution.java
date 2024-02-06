c6eeb3fb7c3aa9ef4f19a69cc6bd8d5e1f2107bb
sidoriak <sidoryak@gmail.com>
2024-02-06 13:25:37 +0700

    task0214

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.pro.task02.task0214;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /* 
@@ -8,8 +11,19 @@ import java.util.Scanner;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = "";
+        String str2 = "";
+        String str3 = "";
+        str1 = reader.readLine();
+        str2 = reader.readLine();
+        str3 = reader.readLine();
+        reader.close();
+        System.out.println(str3);
+        System.out.println(str2.toUpperCase());
+        System.out.println(str1.toLowerCase());
 
     }
 }
