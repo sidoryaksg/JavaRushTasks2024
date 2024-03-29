cf3c39eb7a83556c5671a64420b29bef4d48bb5f
sidoriak <sidoryak@gmail.com>
2024-03-29 23:10:46 +0700

    Task 0517

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.pro.task05.task0517;
+
+import java.util.Arrays;
+
+/* 
+Делим массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[2][];
+    public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        int median;
+        if (array.length % 2 == 0) {
+            median = median = array.length / 2;
+        } else median = array.length / 2 + 1;
+        result[0] = Arrays.copyOfRange(array, 0, median) ;
+        result[1] = Arrays.copyOfRange(array,median, array.length);
+        System.out.println(Arrays.deepToString(result));
+    }
+}
